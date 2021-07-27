66ea3c941429e1515a9dcbaad84a9e290ad06181
Annukul <thakran9.anukul@gmail.com>
2021-07-27 15:15:07 +0530

    Fixed some issues

--- src/pages/home/projects.js
+++ src/pages/home/projects.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Projects = () => {
     const [posts, setPosts] = useState([]);
 
-    const dl = "https://annu-port-api.herokuapp.com//uploads/";
+    const dl = "https://annu-port-api.herokuapp.com/uploads/";
 
     useEffect(() => {
         const fetchPosts = async () => {
