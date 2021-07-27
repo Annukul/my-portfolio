2b1b278286e87906372b099ff179eb4cbf8fc379
Annukul <53966193+Annukul@users.noreply.github.com>
2021-07-27 15:22:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <Router>
       <div>
         <Route path="/" exact render={() => <Home />} />
-        <Route path="/01242289449_create" render={() => <Create />} />
+        <Route path="/01242289449-create" render={() => <Create />} />
         <Route path="/view/:id" render={() => <ViewProject />} />
       </div>
     </Router>
